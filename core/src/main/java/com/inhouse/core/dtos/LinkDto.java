4bfdb8c56ec932a63f67c78662dcf0129d2b623a
Neha Dhiman <ngdhiman88@gmail.com>
2021-07-04 01:05:25 -0600

    Adding Header Component

---
+++ core/src/main/java/com/inhouse/core/dtos/LinkDto.java
@@ -0,0 +1,24 @@
+package com.inhouse.core.dtos;
+
+public class LinkDto {
+    private String PageName;
+    private String PageLinkURL;
+
+
+    public String getPageName() {
+        return PageName;
+    }
+
+    public void setPageName(String pageName) {
+        PageName = pageName;
+    }
+
+    public String getPageLinkURL() {
+        return PageLinkURL;
+    }
+
+    public void setPageLinkURL(String pageLinkURL) {
+        PageLinkURL = pageLinkURL;
+    }
+
+}
